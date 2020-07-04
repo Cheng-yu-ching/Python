d5dc89ad4b022e7ab64e9d406f07005870bc71ee
Cheng-yu-ching <67730100+Cheng-yu-ching@users.noreply.github.com>
2020-07-04 22:48:06 +0800

    上傳示範
    TEST

---
+++ Homework/temp1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+hourly_salary=120 
+#時薪
+annual_salary=hourly_salary*8*365 
+#年收 
+expenses=3000+10000 
+#月支出 
+accumulation=annual_salary-expenses* 12 
+#
+print(accumulation) 
+ 
+
