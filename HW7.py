fa94d69aa190b1b2ee17db97eb3482762eef67fe
Cheng-yu-ching <67730100+Cheng-yu-ching@users.noreply.github.com>
2020-08-20 17:18:11 +0800

    Add files via upload

---
+++ HW7.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 16:14:57 2020
+
+@author: user
+"""
+
+
+
+str1 = "ABC"
+print (str1.lower())
+
+
+str2 ="abc"
+print (str2.upper())
+
+
+str3 ="abc"
+print (str3.title())
+
+
+str4 ="abcDEF"
+print (str4.swapcase())
+
+
+str5 ="abc1234                        5      "
+print (str5.rstrip())
+
+
+str6 ="         abc12345      "
+print (str6.lstrip())
+
+
+print (str6.strip())
+
+
+str7 = "1234"
+print (str7.center(12))
+print (str7.rjust(12))
+print (str7.ljust(12))
+print (str7.zfill(12))
+
