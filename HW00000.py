6e7918e4546c9efda4ae0c02762487fbef03cba3
Cheng-yu-ching <67730100+Cheng-yu-ching@users.noreply.github.com>
2020-08-25 19:11:23 +0800

    Add files via upload

---
+++ HW00000.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 19:09:45 2020
+
+@author: user
+"""
+
+
+for n in range(1,101,2):
+    if n % 2 !=0:
+        print(n+1)
+m=1
+while m in range(1,101,2):
+    print(m+1)
+    m += 2
+
+a = int(input())
+for n in range(1,a,2):
+    print(n+1)
+m=1
+while m in range(1,a,2):
+    print(m+1)
+    m += 2
